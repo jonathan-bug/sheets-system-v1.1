73314bd4d0fe45e7e3132ef1c2793c31e6854249
jonathan-bug <jonathanlima.dev@gmail.com>
2025-04-07 14:40:47 -0400

    Periods model added

---
+++ app/Models/Period.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Period extends Model
+{
+    protected $table = 'periods';
+    protected $fillable = [
+        'month',
+        'year'
+    ];
+}
