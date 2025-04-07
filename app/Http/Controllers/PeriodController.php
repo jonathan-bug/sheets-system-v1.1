3bdc709a36581adf40c35214030152662c8b3d1f
jonathan-bug <jonathanlima.dev@gmail.com>
2025-04-07 16:37:41 -0400

    Periods CRUD added

---
+++ app/Http/Controllers/PeriodController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Period;
+
+class PeriodController extends Controller
+{
+    public function page() {
+        return view('pages.periods.index');
+    }
+
+    public function index() {
+        $periods = Period::all();
+
+        return $periods;
+    }
+
+    public function store(Request $request) {
+        $validated = Validator::make($request->all(), [
+            'month' => 'required',
+            'year' => 'required'
+        ]);
+
+        if($validated->fails()) {
+            $data = [
+                'message' => 'Unable to validate',
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $record = Period::create($request->all());
+        
+        $data = [
+            'message' => 'Period added successfully',
+            'record' => $record,
+            'state' => 200
+        ];
+
+        return $data;
+    }
+
+    public function destroy($id) {
+        $period = Period::find($id);
+        $period->delete();
+
+        $data = [
+            'message' => 'Period deleted',
+            'status' => 200
+        ];
+
+        return $data;
+    }
+
+    public function update(Request $request, $id) {
+        $validated = Validator::make($request->all(), [
+            'month' => 'required',
+            'year' => 'required'
+        ]);
+
+        if($validated->fails()) {
+            $data = [
+                'message' => 'Unable to validate',
+                'errors' => $validated->errors(),
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $period = Period::find($id);
+
+        if(!$period) {
+            $data = [
+                'message' => 'Period not found',
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $period->month = $request->get('month');
+        $period->year = $request->get('year');
+        $period->save();
+
+        $data = [
+            'message' => 'Period updated successfully',
+            'record' => $period,
+            'status' => 200
+        ];
+
+        return $data;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PeriodController;
 use App\Http\Middleware\AuthUser;
 
 // auth
@@ -23,4 +24,17 @@ Route::middleware(AuthUser::class)->group(function () {
     Route::post('/api/employees', [EmployeeController::class, 'store'])->name('api.employees.store');
     Route::delete('/api/employees', [EmployeeController::class, 'destroy'])->name('api.employees.destroy');
     Route::put('/api/employees', [EmployeeController::class, 'update'])->name('api.employees.update');
+
+});
+
+
+// period
+Route::get('/periods', [PeriodController::class, 'page'])->name('periods');
+Route::get('/api/periods', [PeriodController::class, 'index'])->name('api.periods.index');
+Route::post('/api/periods', [PeriodController::class, 'store'])->name('api.periods.store');
+Route::delete('/api/periods/{id}', [PeriodController::class, 'destroy'])->name('api.periods.destroy');
+Route::put('/api/periods/{id}', [PeriodController::class, 'update'])->name('api.periods.update');
+
+Route::get('/token', function () {
+    return csrf_token();
 });
