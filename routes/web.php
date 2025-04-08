689727001ebcd535cf82acdf9eaf466e867c73cd
jonathan-bug <jonathanlima.dev@gmail.com>
2025-04-08 12:26:19 -0400

    Salaries table structure added

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,14 @@ Route::middleware(AuthUser::class)->group(function () {
     Route::delete('/api/employees', [EmployeeController::class, 'destroy'])->name('api.employees.destroy');
     Route::put('/api/employees', [EmployeeController::class, 'update'])->name('api.employees.update');
 
+    // period
+    Route::get('/periods', [PeriodController::class, 'page'])->name('periods');
+    Route::get('/api/periods', [PeriodController::class, 'index'])->name('api.periods.index');
+    Route::get('/api/periods/{id}', [PeriodController::class, 'find'])->name('api.periods.find');
+    Route::post('/api/periods', [PeriodController::class, 'store'])->name('api.periods.store');
+    Route::delete('/api/periods/{id}', [PeriodController::class, 'destroy'])->name('api.periods.destroy');
+    Route::put('/api/periods/{id}', [PeriodController::class, 'update'])->name('api.periods.update');
 });
-
-
-// period
-Route::get('/periods', [PeriodController::class, 'page'])->name('periods');
-Route::get('/api/periods', [PeriodController::class, 'index'])->name('api.periods.index');
-Route::get('/api/periods/{id}', [PeriodController::class, 'find'])->name('api.periods.find');
-Route::post('/api/periods', [PeriodController::class, 'store'])->name('api.periods.store');
-Route::delete('/api/periods/{id}', [PeriodController::class, 'destroy'])->name('api.periods.destroy');
-Route::put('/api/periods/{id}', [PeriodController::class, 'update'])->name('api.periods.update');
 
 Route::get('/token', function () {
     return csrf_token();
