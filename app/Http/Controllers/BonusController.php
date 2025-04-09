1c5af5414f964b49da3f3329f75aebdb844e5b8e
jonathan-bug <jonathanlima.dev@gmail.com>
2025-04-08 22:36:25 -0400

    Bonus CRUD added

---
+++ app/Http/Controllers/BonusController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Employee;
+use App\Models\Bonus;
+
+class BonusController extends Controller
+{
+    public function index($dui) {
+        $employee = Employee::find($dui);
+        $bonus = $employee->bonus;
+
+        return $bonus;
+    }
+
+    public function destroy($id) {
+        $bonus = Bonus::find($id);
+
+        if(!$bonus) {
+            $data = [
+                'message' => 'Bonus not found',
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $bonus->delete();
+
+        $data = [
+            'message' => 'Bonus deleted',
+            'status' => 200
+        ];
+
+        return $data;
+    }
+
+    public function store(Request $request) {
+        $validated = Validator::make($request->all(), [
+            'employee_dui' => 'required',
+            'amount' => 'required'
+        ]);
+
+        if($validated->fails()) {
+            $data = [
+                'message' => 'Unable to validate',
+                'erros' => $validated->errors(),
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $bonus = Bonus::create($request->all());
+
+        $data = [
+            'message' => 'Bonus added successfully',
+            'record' => $bonus,
+            'status' => 200
+        ];
+
+        return $data;
+    }
+
+    public function update(Request $request, $id) {
+        $validated = Validator::make($request->all(), [
+            'employee_dui' => 'required',
+            'amount' => 'required'
+        ]);
+
+        if($validated->fails()) {
+            $data = [
+                'message' => 'Unable to validate',
+                'erros' => $validated->errors(),
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $bonus = Bonus::find($id);
+
+        if(!$bonus) {
+            $data = [
+                'message' => 'Bonus not found',
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $bonus->employee_dui = $request->get('employee_dui');
+        $bonus->amount = $request->get('amount');
+        $bonus->save();
+
+        $data = [
+            'message' => 'Bonus updated successfully',
+            'record' => $bonus,
+            'status' => 200
+        ];
+
+        return $data;
+    }
+
+    public function find($id) {
+        $bonus = Bonus::find($id);
+
+        if(!$bonus) {
+            $data = [
+                'message' => 'Bonus not found',
+                'status' => 400
+            ];
+
+            return $data;
+        }
+
+        $data = [
+            'message' => 'Bonus found',
+            'record' => $bonus,
+            'status' => 200
+        ];
+
+        return $data;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\PeriodController;
 use App\Http\Controllers\SalaryController;
 use App\Http\Controllers\HourController;
+use App\Http\Controllers\BonusController;
 use App\Http\Middleware\AuthUser;
 
 // auth
@@ -42,15 +43,22 @@ Route::middleware(AuthUser::class)->group(function () {
     Route::delete('/api/salaries/{id}', [SalaryController::class, 'destroy'])->name('api.salaries.destroy');
     Route::post('/api/salaries', [SalaryController::class, 'store'])->name('api.salaries.store');
     Route::put('/api/salaries/{id}', [SalaryController::class, 'update'])->name('api.salaries.update');
+
+    // hours
+    Route::get('/hours/{dui}', [HourController::class, 'page'])->name('hours');
+    Route::get('/api/hours/{dui}', [HourController::class, 'index'])->name('api.hours.index');
+    Route::get('/api/hours/find/{id}', [HourController::class, 'find'])->name('api.hours.find');
+    Route::delete('/api/hours/{id}', [HourController::class, 'destroy'])->name('api.hours.destroy');
+    Route::post('/api/hours', [HourController::class, 'store'])->name('api.hours.store');
+    Route::put('/api/hours/{id}', [HourController::class, 'update'])->name('api.hours.update');
 });
 
-// hours
-Route::get('/hours/{dui}', [HourController::class, 'page'])->name('hours');
-Route::get('/api/hours/{dui}', [HourController::class, 'index'])->name('api.hours.index');
-Route::get('/api/hours/find/{id}', [HourController::class, 'find'])->name('api.hours.find');
-Route::delete('/api/hours/{id}', [HourController::class, 'destroy'])->name('api.hours.destroy');
-Route::post('/api/hours', [HourController::class, 'store'])->name('api.hours.store');
-Route::put('/api/hours/{id}', [HourController::class, 'update'])->name('api.hours.update');
+// bonus
+Route::get('/api/bonus/{dui}', [BonusController::class, 'index'])->name('api.bonus.index');
+Route::get('/api/bonus/find/{id}', [BonusController::class, 'find'])->name('api.bonus.find');
+Route::delete('/api/bonus/{id}', [BonusController::class, 'destroy'])->name('api.bonus.destroy');
+Route::post('/api/bonus', [BonusController::class, 'store'])->name('api.bonus.store');
+Route::put('/api/bonus/{id}', [BonusController::class, 'update'])->name('api.bonus.update');
 
 Route::get('/token', function () {
     return csrf_token();
